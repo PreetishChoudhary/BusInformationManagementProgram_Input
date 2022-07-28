61e31a6acc7c025b376cb8d2a2de8555213c3411
Guitarband <60247328+Guitarband@users.noreply.github.com>
2022-07-28 15:30:40 +1000

    Updated xml writing to fix atSchool boolean not changing

--- BusInformationManagementProgram_Input/Form1.cs
+++ BusInformationManagementProgram_Input/Form1.cs
@@ -28,6 +28,11 @@ namespace BusInformationManagementProgram_Input
             InitializeComponent();
         }
 
+        /// <summary>
+        /// The ReadFile module reads a specific file depending on the given bus number and type. The module saves the information from the file and stores it in a record.
+        /// </summary>
+        /// <param name="busNumber">The number of the bus being specified.</param>
+        /// <param name="type">The type of bus being specified, either 'school' or 'sport'.</param>
         public void ReadFile(int busNumber, string type)
         {
             if (type == "school")
@@ -64,6 +69,15 @@ namespace BusInformationManagementProgram_Input
                 index++;
             }
         }
+
+        /// <summary>
+        /// The CreateRecord module creates a new entry for a specified bus. The module adds to the xml document of the specified bus along with appending the new information in the respective bus record.
+        /// </summary>
+        /// <param name="busNumber"></param>
+        /// <param name="time"></param>
+        /// <param name="date"></param>
+        /// <param name="status"></param>
+        /// <param name="type"></param>
         void CreateRecord(string busNumber, string time, string date, string status, string type)
         {
             if (type == "school")
@@ -97,38 +111,95 @@ namespace BusInformationManagementProgram_Input
             b.arrivalTime = 0;
             b.departTime = 0;
             busData.AppendChild(atSchool);
-            busData.AppendChild(arrivalTime);
-            busData.AppendChild(departureTime);
 
-            if (status == "In")
+            if (type == "SchoolBuses/")
             {
-                b.atSchool = false;
-                departureTime.InnerText = time;
-                atSchool.InnerText = "False";
-                b.departTime = Convert.ToInt32(time);
-                root.LastChild.LastChild.InnerText = time;
-                busData.AppendChild(atSchool);
-                busData.AppendChild(departureTime);
-            }
-
-            if (status == "Out" || status == "none")
-            {
-                b.atSchool = true;
-                busData.SetAttribute("date", date);
-                root.AppendChild(busData);
-                arrivalTime.InnerText = time;
-                atSchool.InnerText = "True";
-                b.arrivalTime = Convert.ToInt32(time);
-                busData.AppendChild(atSchool);
                 busData.AppendChild(arrivalTime);
                 busData.AppendChild(departureTime);
+
+                if (status == "In")
+                {
+                    b.atSchool = false;
+                    departureTime.InnerText = time;
+                    atSchool.InnerText = "False";
+                    b.departTime = Convert.ToInt32(time);
+                    root.LastChild.FirstChild.InnerText = "False";
+                    root.LastChild.LastChild.InnerText = time;
+                    busData.AppendChild(atSchool);
+                    busData.AppendChild(departureTime);
+
+                    testRecord[testRecord.Length - 1].atSchool = b.atSchool;
+                    testRecord[testRecord.Length - 1].arrivalTime = b.arrivalTime;
+
+                    busRecordArray[Convert.ToInt32(busNumber)] = testRecord;
+                }
+
+                if (status == "none")
+                {
+                    b.atSchool = true;
+                    busData.SetAttribute("date", date);
+                    root.AppendChild(busData);
+                    arrivalTime.InnerText = time;
+                    atSchool.InnerText = "True";
+                    b.arrivalTime = Convert.ToInt32(time);
+                    busData.AppendChild(atSchool);
+                    busData.AppendChild(arrivalTime);
+                    busData.AppendChild(departureTime);
+
+                    record[record.Length - 1] = b;
+
+                    busRecordArray[Convert.ToInt32(busNumber)] = record;
+                }
+            }
+            else
+            {
+                busData.AppendChild(departureTime);
+                busData.AppendChild(arrivalTime);
+
+                if (status == "Out")
+                {
+                    b.atSchool = true;
+                    arrivalTime.InnerText = time;
+                    atSchool.InnerText = "True";
+                    b.arrivalTime = Convert.ToInt32(time);
+                    root.LastChild.FirstChild.InnerText = "False";
+                    root.LastChild.LastChild.InnerText = time;
+                    busData.AppendChild(atSchool);
+                    busData.AppendChild(arrivalTime);
+
+                    testRecord[testRecord.Length - 1].atSchool = b.atSchool;
+                    testRecord[testRecord.Length - 1].arrivalTime = b.arrivalTime;
+
+                    sportBusRecordArray[Convert.ToInt32(busNumber)] = testRecord;
+                }
+
+                if (status == "none")
+                {
+                    b.atSchool = false;
+                    busData.SetAttribute("date", date);
+                    root.AppendChild(busData);
+                    departureTime.InnerText = time;
+                    atSchool.InnerText = "False";
+                    b.departTime = Convert.ToInt32(time);
+                    busData.AppendChild(atSchool);
+                    busData.AppendChild(departureTime);
+                    busData.AppendChild(arrivalTime);
+
+                    record[record.Length - 1] = b;
+
+                    sportBusRecordArray[Convert.ToInt32(busNumber)] = record;
+                }
             }
 
-            record[record.Length - 1] = b;
-            busRecordArray[Convert.ToInt32(busNumber)] = record;
             doc.Save(type + "BusData_" + b.busNumber + ".xml");
         }
 
+        /// <summary>
+        /// The CheckStatus module retrieves the record for a specified bus number and searches for an object containing a specified date. If it is able to find an object containing the date, it returns the value of the atSchool property of the object, otherwise it returns 'none'.
+        /// </summary>
+        /// <param name="busNumber">The number of the bus being specified.</param>
+        /// <param name="date">The date being specified.</param>
+        /// <returns></returns>
         string CheckStatus(int busNumber, string date)
         {
             BusRecord[] record = busRecordArray[busNumber];
@@ -151,6 +222,10 @@ namespace BusInformationManagementProgram_Input
             return "none";
         }
 
+        /// <summary>
+        /// The FillBusComboBox module reads a file containing each individual bus number and places it in a comboBox.
+        /// </summary>
+        /// <param name="filePath">The path for the file being read.</param>
         void FillBusComboBox(string filePath)
         {
             cmbBxBusNumber.Items.Clear();
@@ -198,6 +273,26 @@ namespace BusInformationManagementProgram_Input
                 type = "sport";
             }
             CreateRecord(cmbBxBusNumber.SelectedIndex.ToString(), DateTime.Now.ToString("HHmm"), DateTime.Now.ToString("ddMMyy"), status, type);
+
+            if (status == "none")
+            {
+                if (cmbBxBusType.SelectedIndex == 0)
+                {
+                    btnCheck.Text = "Check Out";
+                }
+                else
+                {
+                    btnCheck.Text = "Check In";
+                }
+            }
+            else if (status == "In")
+            {
+                btnCheck.Text = "Check In";
+            }
+            else
+            {
+                btnCheck.Text = "Check Out";
+            }
         }
 
         private void cmbBxBusNumber_SelectedIndexChanged(object sender, EventArgs e)
@@ -208,9 +303,20 @@ namespace BusInformationManagementProgram_Input
             currentTime.Text = time;
             currentDate.Text = date;
             string status = CheckStatus(Convert.ToInt32(cmbBxBusNumber.SelectedIndex.ToString()), DateTime.Now.ToString("ddMMyy"));
-            if(status == "In")
+            if(status == "none")
             {
-                btnCheck.Text = "Check Out";
+                if (cmbBxBusType.SelectedIndex == 1)
+                {
+                    btnCheck.Text = "Check Out";
+                }
+                else
+                {
+                    btnCheck.Text = "Check In";
+                }
+            }
+            else if(status == "In")
+            {
+                 btnCheck.Text = "Check Out";
             }
             else
             {
